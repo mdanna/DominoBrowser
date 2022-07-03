16e7603bc11fa482c88b3c0b76a22ee976f7b73f
Mario D'Anna <mario.danna@hcl.com>
2022-07-03 15:31:26 +0200

    Finished the week browser component

---
+++ modules/dateUtils.js
@@ -0,0 +1,124 @@
+const dateUtils = {
+    MILLISEC_PER_DAY: 24 * 60 * 60 * 1000,
+
+    getFirstMonday(year){
+        const oneJan = new Date(year, 0, 1);
+        const oneJanDay = oneJan.getDay();
+        const dayToFirstMonaday = (8 - oneJanDay) % 7;
+        return new Date(year, 0, 1 + dayToFirstMonaday);
+    },
+
+    getDateInfo(year, month, day){
+        const inputDate = new Date(year, month - 1, day);
+        const dayOfWeek = inputDate.getDay();
+        const firstMonday = dateUtils.getFirstMonday(year);
+        var numberOfDays = parseInt(Math.floor((inputDate - firstMonday) / dateUtils.MILLISEC_PER_DAY));
+        var weekNumber = inputDate >= firstMonday ? Math.ceil((dayOfWeek + 1 + numberOfDays) / 7) : 0;
+
+        return {
+            date: `${day}/${month}/${year}`,
+            dayOfWeek,
+            weekNumber,
+            weekDays: dateUtils.getWeekDays(year, weekNumber)
+        };
+    },
+
+    getWeekDays(year, weekNumber) {
+        if(weekNumber > 0){
+            const weekDays = [];
+            const firstMonday = dateUtils.getFirstMonday(year);
+            const mondayOfWeek = new Date(firstMonday.valueOf() + (7 * (weekNumber - 1) * dateUtils.MILLISEC_PER_DAY));
+
+            for(let i = 0; i < 7; i++){
+                let dayOfWeek = new Date(mondayOfWeek.valueOf() + (i * dateUtils.MILLISEC_PER_DAY));
+                weekDays.push({
+                    year: dayOfWeek.getFullYear(),
+                    month: dayOfWeek.getMonth() + 1,
+                    day: dayOfWeek.getDate(),
+                    dayOfWeek: dayOfWeek.getDay()
+                });
+            }
+
+            return weekDays;
+        } else {
+            return dateUtils.getWeekDays(year - 1, 52);
+        }
+    },
+  
+  getWeekDay(weekDay){
+    let ret;
+    switch(weekDay){
+      case 1:
+        ret = 'Monday';
+        break;
+       case 2:
+        ret = 'Tuesday';
+        break;
+       case 3:
+        ret = 'Wednesday';
+        break;
+       case 4:
+        ret = 'Thursday';
+        break;
+       case 5:
+        ret = 'Friday';
+        break;
+       case 6:
+        ret = 'Saturday';
+        break;
+      default:
+        ret = 'Sunday';
+        break;
+    }
+    return ret;
+  },
+  
+    getMonth(month){
+    let ret;
+    switch(month){
+      case 1:
+        ret = 'January';
+        break;
+       case 2:
+        ret = 'February';
+        break;
+       case 3:
+        ret = 'March';
+        break;
+       case 4:
+        ret = 'April';
+        break;
+       case 5:
+        ret = 'May';
+        break;
+       case 6:
+        ret = 'June';
+        break;
+      case 7:
+        ret = 'July';
+        break;
+       case 8:
+        ret = 'August';
+        break;
+       case 9:
+        ret = 'September';
+        break;
+       case 10:
+        ret = 'October';
+        break;
+       case 11:
+        ret = 'November';
+        break;
+      default:
+        ret = 'December';
+        break;
+    }
+    return ret;
+  }
+
+  
+  
+    
+
+};
+
---
+++ userwidgets/com.hcl.voessing.WeekView/modules/require/WeekViewController.js
@@ -0,0 +1,115 @@
+define(function() {
+
+  return {
+    weekNumber: null,
+    year: null,
+
+    constructor: function(baseConfig, layoutConfig, pspConfig) {
+      this.view.preShow = () => {
+        if(!this.initDone){
+          this.initMonthDayButtons();
+          this.initTodayButton();
+          this.initFlxArrowLeft();
+          this.initFlxArrowRight();
+          this.initDone = true;
+        }
+      };
+    },
+
+    initMonthDayButtons(){
+      for(let i = 1; i <= 7; i++){
+
+        this.view[`monthDay${i}`].onClick = () => {
+
+          this.view.lblDay.text = this.view[`monthDay${i}`].day;
+          this.view.lblWeekday.text = dateUtils.getWeekDay(new Date(parseInt(this.view[`monthDay${i}`].year), 
+                                                                    parseInt(this.view[`monthDay${i}`].month - 1), 
+                                                                    parseInt(this.view[`monthDay${i}`].day))
+                                                           .getDay()).substring(0, 3);
+          this.view.lblMonthYear.text = `${dateUtils.getMonth(parseInt(this.view[`monthDay${i}`].month))} ${this.view[`monthDay${i}`].year}`;
+
+
+          for(let j = 1; j <= 7; j++){
+            if(j === i){
+              this.view[`monthDay${j}`].skinDay = 'sknLblWhite80';
+              this.view[`monthDay${j}`].skinWeekday = 'sknLblWhite80'; 
+              this.view[`monthDay${j}`].skinMonthDay = 'sknFlxButtonBlue';
+            } else {
+              this.view[`monthDay${j}`].skinDay = 'sknLblGrey80';
+              this.view[`monthDay${j}`].skinWeekday = 'sknLblGrey80'; 
+              this.view[`monthDay${j}`].skinMonthDay = 'sknFlxWhite';
+            }
+          }
+        };
+      }
+
+    },
+
+    initTodayButton() {
+      this.view.buttonToday.onButtonClick = () => {
+        const today = new Date();
+        this.focusDate = `${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}`;
+      };
+    },
+
+    initFlxArrowLeft() {
+      this.view.flxArrowLeft.onClick = () => {
+        this.year = this.weekNumber ? this.year : this.year - 1;
+        this.weekNumber = this.weekNumber ? this.weekNumber - 1 : 51; //week 0 = week 52 of previous year
+        const dateInfoPrevMonday = dateUtils.getWeekDays(this.year, this.weekNumber)[0]; 
+        this.focusDate = `${dateInfoPrevMonday.day}/${dateInfoPrevMonday.month}/${dateInfoPrevMonday.year}`;
+      };
+    },
+
+    initFlxArrowRight() {
+      this.view.flxArrowRight.onClick = () => {
+        this.year = this.weekNumber === 52 ? this.year + 1: this.year;
+        this.weekNumber = this.weekNumber === 52 ? 1 : this.weekNumber + 1; //week 0 = week 52 of previous year
+        const dateInfoNextMonday = dateUtils.getWeekDays(this.year, this.weekNumber)[0]; 
+        this.focusDate = `${dateInfoNextMonday.day}/${dateInfoNextMonday.month}/${dateInfoNextMonday.year}`;
+      };
+    },
+
+    //Logic for getters/setters of custom properties
+    initGettersSetters: function() {
+      defineGetter(this, 'focusDate', () => {
+        return this._focusDate;
+      });
+
+      defineSetter(this, 'focusDate', value => {
+        if(value){
+          this._focusDate = value;
+        } else {
+          const today = new Date();
+          this._focusDate = `${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}`;
+        }
+        const dateComponents = this._focusDate.split('/');
+        const {weekNumber, weekDays} = dateUtils.getDateInfo(dateComponents[2], dateComponents[1], dateComponents[0]);
+        this.weekNumber = weekNumber;
+        this.year = dateComponents[2];
+
+        this.view.lblDay.text = dateComponents[0];
+        this.view.lblWeekday.text = dateUtils.getWeekDay(new Date(dateComponents[2], dateComponents[1] - 1, dateComponents[0]).getDay()).substring(0, 3);
+        this.view.lblMonthYear.text = `${dateUtils.getMonth(parseInt(dateComponents[1]))} ${dateComponents[2]}`;
+
+        for(let i = 0; i < 7; i++){
+          const monthDay = this.view[`monthDay${i + 1}`];
+          monthDay.weekDay = dateUtils.getWeekDay(weekDays[i].dayOfWeek).substring(0, 1);
+          monthDay.day = weekDays[i].day + '';
+          monthDay.month = weekDays[i].month;
+          monthDay.year = weekDays[i].year;
+
+          if(monthDay.day === dateComponents[0]){
+            monthDay.skinDay = 'sknLblWhite80';
+            monthDay.skinWeekday = 'sknLblWhite80'; 
+            monthDay.skinMonthDay = 'sknFlxButtonBlue';
+          } else {
+            monthDay.skinDay = 'sknLblGrey80';
+            monthDay.skinWeekday = 'sknLblGrey80'; 
+            monthDay.skinMonthDay = 'sknFlxWhite';
+          }
+        }
+      });
+    }
+  };
+});
